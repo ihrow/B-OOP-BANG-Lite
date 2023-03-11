48845a00765ff76d410f07b578749bf118606ea8
igor <igornotfound@gmail.com>
2023-03-11 21:33:43 +0100

    Implemented basic ctor for Board class

--- src/main/java/sk/stuba/fei/uim/oop/board/Board.java
+++ src/main/java/sk/stuba/fei/uim/oop/board/Board.java
@@ -1,4 +1,21 @@
 package sk.stuba.fei.uim.oop.board;
 
+import sk.stuba.fei.uim.oop.deck.Deck;
+import sk.stuba.fei.uim.oop.player.Player;
+
 public class Board {
+    private Deck deck;
+
+    public Board(Player[] players) {
+        this.deck = new Deck();
+        this.deck.shuffle();
+
+        // Each player starts with 4 cards from the deck
+        for (Player player : players) {
+            for (int i = 0; i < 4; i++) {
+                player.addCard(this.deck.draw());
+            }
+        }
+
+    }
 }
