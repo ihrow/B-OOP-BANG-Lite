845b4d2d976774e5b1dd2fea4bcb229f8088ccb7
igor <igornotfound@gmail.com>
2023-03-11 22:52:36 +0100

    Added ANSI color codes

---
+++ src/main/java/sk/stuba/fei/uim/oop/utility/Colors.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.utility;
+
+public class Colors {
+    public static final String ANSI_RESET = "\033[0m";
+    public static final String ANSI_RED = "\033[0;31m";
+    public static final String ANSI_RED_B = "\033[31;1m";
+    public static final String ANSI_GREEN = "\033[0;32m";
+    public static final String ANSI_YELLOW = "\033[0;33m";
+    public static final String ANSI_YELLOW_BI = "\033[33;1;3m";
+    public static final String ANSI_BLUE = "\033[0;34m";
+    public static final String ANSI_PURPLE = "\033[0;35m";
+    public static final String ANSI_CYAN = "\033[0;36m";
+}
