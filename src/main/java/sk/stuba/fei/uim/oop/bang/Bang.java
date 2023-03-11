fd0b3ec1ae17745df8f3ee602c71d7fb5b15f9a0
igor <igornotfound@gmail.com>
2023-03-11 19:15:13 +0100

    Implemented start of the game by obtaining the number of players and their names

--- src/main/java/sk/stuba/fei/uim/oop/bang/Bang.java
+++ src/main/java/sk/stuba/fei/uim/oop/bang/Bang.java
@@ -1,4 +1,21 @@
 package sk.stuba.fei.uim.oop.bang;
 
+import sk.stuba.fei.uim.oop.player.Player;
+import sk.stuba.fei.uim.oop.utility.KeyboardInput;
+import sk.stuba.fei.uim.oop.utility.ZKlavesnice;
+
 public class Bang {
+    private final Player[] players;
+
+    public Bang() {
+        System.out.println("\uD83C\uDF35 Bang! Bang! Bang! \uD83D\uDCA5 \n");
+        int numberOfPlayers = 0;
+        while (numberOfPlayers < 2 || numberOfPlayers > 4) {
+            numberOfPlayers = KeyboardInput.readInt("\uD83E\uDD20 Enter amount of players (2-4)");
+        }
+        this.players = new Player[numberOfPlayers];
+        for (int i = 0; i < numberOfPlayers; i++) {
+            this.players[i] = new Player(KeyboardInput.readString("Enter " + (i + 1) + " player name"));
+        }
+    }
 }
