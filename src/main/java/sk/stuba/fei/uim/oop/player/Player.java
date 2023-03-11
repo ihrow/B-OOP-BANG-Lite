773b225bfd24fb4b614c35fd7431d7e96e0a9807
igor <igornotfound@gmail.com>
2023-03-11 18:38:30 +0100

    Added basic fields and getters for Player Class

--- src/main/java/sk/stuba/fei/uim/oop/player/Player.java
+++ src/main/java/sk/stuba/fei/uim/oop/player/Player.java
@@ -1,4 +1,29 @@
 package sk.stuba.fei.uim.oop.player;
 
+import sk.stuba.fei.uim.oop.cards.Card;
+import java.util.ArrayList;
+
 public class Player {
+
+    private final String name;
+    private int lives;
+    private ArrayList<Card> cards;
+
+    public Player(String name) {
+        this.name = name;
+        this.lives = 4;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return lives;
+    }
+
+    public ArrayList<Card> getAllCards() {
+        return cards;
+    }
+
 }
