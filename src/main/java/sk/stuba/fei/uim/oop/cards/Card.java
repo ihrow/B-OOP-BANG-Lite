dc8b4ba7aebc85566aa643c60b8aa4e0b609612e
igor <igornotfound@gmail.com>
2023-03-11 21:08:36 +0100

    Added basic fields, ctor and getters for Card Class

--- src/main/java/sk/stuba/fei/uim/oop/cards/Card.java
+++ src/main/java/sk/stuba/fei/uim/oop/cards/Card.java
@@ -1,4 +1,14 @@
 package sk.stuba.fei.uim.oop.cards;
 
-public class Card {
+public abstract class Card {
+    protected String name;
+
+    public Card(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
