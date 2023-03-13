03018bf3e84146680a94254371e5e6f142a94461
igor <igornotfound@gmail.com>
2023-03-13 23:51:11 +0100

    added some colors

--- src/main/java/sk/stuba/fei/uim/oop/utility/Colors.java
+++ src/main/java/sk/stuba/fei/uim/oop/utility/Colors.java
@@ -6,8 +6,11 @@ public class Colors {
     public static final String ANSI_RED_B = "\033[31;1m";
     public static final String ANSI_GREEN = "\033[0;32m";
     public static final String ANSI_YELLOW = "\033[0;33m";
+    public static final String ANSI_YELLOW_B = "\033[33;1m";
     public static final String ANSI_YELLOW_BI = "\033[33;1;3m";
     public static final String ANSI_BLUE = "\033[0;34m";
+
+    public static final String ANSI_BLUE_B = "\033[34;1m";
     public static final String ANSI_PURPLE = "\033[0;35m";
     public static final String ANSI_CYAN = "\033[0;36m";
 }
