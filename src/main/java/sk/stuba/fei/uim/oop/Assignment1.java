037694737ba0c4a3dbcebbc613b4b6a9d9fac3c4
igor <igornotfound@gmail.com>
2023-03-14 16:21:14 +0100

    some comments

--- src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
+++ src/main/java/sk/stuba/fei/uim/oop/Assignment1.java
@@ -4,6 +4,10 @@ import sk.stuba.fei.uim.oop.bang.Game;
 
 public class Assignment1 {
     public static void main(String[] args) {
+        /*
+            Great inspiration from the repository
+            https://github.com/TomasKovyKovacik/B-OOP-2022-StreleneKacky
+         */
         Game bang = new Game();
     }
 }
