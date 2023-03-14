6bddcccd0de2a157ff50c7df8626fe1c750c3f86
igor <igornotfound@gmail.com>
2023-03-14 15:56:36 +0100

    super easy implementation of Indians card

--- src/main/java/sk/stuba/fei/uim/oop/cards/Indians.java
+++ src/main/java/sk/stuba/fei/uim/oop/cards/Indians.java
@@ -5,10 +5,11 @@ import sk.stuba.fei.uim.oop.player.Player;
 import static sk.stuba.fei.uim.oop.utility.Colors.*;
 
 
-public class Indians extends Card {
+public class Indians extends Bang {
     public static final String CARD_NAME = ANSI_YELLOW_B + "Indians" + ANSI_RESET;
     public Indians(Deck deck) {
-        super(CARD_NAME, deck);
+        super(deck);
+        this.name = CARD_NAME;
     }
     @Override
     public void play(Player targetPlayer) {
